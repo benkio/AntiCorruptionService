f2905bfea2f881abb1be981b8ed1aa607b5ea2ef
Enrico Benini <ebenini@mdsol.com>
2019-12-06 17:53:30 +0000

    Conventions

--- src/Api/Domain/ExternalService.hs
+++ src/Api/Domain/ExternalService.hs
@@ -9,46 +9,46 @@ import Data.Text
 import Data.Aeson
 
 data Beer = Beer {
-  id :: Int,
-  name :: !Text,
-  tagline :: !Text,
-  first_brewed :: !Text,
-  description :: !Text,
-  image_url :: Maybe Text,
-  abv :: Int,
-  ibu :: Maybe Int,
-  target_fg :: Int,
-  target_og :: Int,
-  ebc :: Int,
-  srm :: Double,
-  ph :: Double,
-  attenuation_level :: Int,
-  volume :: Volume,
-  boil_volume :: Volume,
-  method :: Method,
-  ingredients :: Ingredients,
-  food_pairing :: [Text],
-  brewers_tips :: !Text,
-  contributed_by :: !Text
+   id                :: Int
+ , name              :: !Text
+ , tagline           :: !Text
+ , first_brewed      :: !Text
+ , description       :: !Text
+ , image_url         :: Maybe Text
+ , abv               :: Int
+ , ibu               :: Maybe Int
+ , target_fg         :: Int
+ , target_og         :: Int
+ , ebc               :: Int
+ , srm               :: Double
+ , ph                :: Double
+ , attenuation_level :: Int
+ , volume            :: Volume
+ , boil_volume       :: Volume
+ , method            :: Method
+ , ingredients       :: Ingredients
+ , food_pairing      :: [Text]
+ , brewers_tips      :: !Text
+ , contributed_by    :: !Text
 } deriving (Show, Generic, FromJSON, ToJSON)
 
 newtype Volume = Volume UnitValue deriving (Show, Generic, FromJSON, ToJSON)
 newtype Temperature = Temperature UnitValue deriving (Show, Generic, FromJSON, ToJSON)
 
 data UnitValue = UnitValue {
-  value :: Double,
-  unit :: !Text
+  value :: Double
+, unit  :: !Text
   } deriving (Show, Generic, FromJSON, ToJSON)
 
 data Method = Method {
-  mash_temp :: [MashTemperature],
-  fermentation :: Fermentation,
-  twist :: Maybe Text
+  mash_temp    :: [MashTemperature]
+, fermentation :: Fermentation
+, twist        :: Maybe Text
   } deriving (Show, Generic, FromJSON, ToJSON)
 
 data MashTemperature = MashTemperature {
-  temp :: Temperature,
-  duration :: Maybe Int
+  temp :: Temperature
+,  duration :: Maybe Int
   }  deriving (Show, Generic, FromJSON, ToJSON)
 
 data Fermentation = Fermentation {
@@ -56,19 +56,19 @@ data Fermentation = Fermentation {
   }  deriving (Show, Generic, FromJSON, ToJSON)
 
 data Ingredients = Ingredients {
-  malt :: [Malt],
-  hops :: [Hops],
-  yeast :: Text
+   malt :: [Malt]
+,  hops :: [Hops]
+,  yeast :: Text
   } deriving (Show, Generic, FromJSON, ToJSON)
 
 data Malt = Malt {
-  name :: Text,
-  amount :: UnitValue
+   name :: Text
+,  amount :: UnitValue
   }  deriving (Show, Generic, FromJSON, ToJSON)
 
 data Hops = Hops {
-  name :: Text,
-  amount :: UnitValue,
-  add :: Text,
-  attribute :: Text
+   name :: Text
+,  amount :: UnitValue
+,  add :: Text
+,  attribute :: Text
   }  deriving (Show, Generic, FromJSON, ToJSON)
